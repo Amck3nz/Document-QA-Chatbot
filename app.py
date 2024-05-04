50f3b3ba635ce0d727a797cc7525d34ebffdf634
Amck3nz <amckenzie18@outlook.com>
2024-05-03 20:25:31 -0400

    basic structure and initial code added

--- app.py
+++ app.py
@@ -43,16 +43,27 @@ def vector_embedding():
     """
     1: Data Ingestion --> 2: Document Loading --> 3: Chunking --> 4: Splitting --> 5: Create Vector Store
     """
-    # Embeddings will be used elsewhere so good to use sessions
-    st.session_state.embeddings = OpenAIEmbeddings()
-    st.session_state.loader = PyPDFDirectoryLoader("./FOLDER_NAME")                                                # 1
-    st.session_state.docs = st.session_state.loader.load()                                                         # 2
-    # Need to also save the resulting chunks to session
-    st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)            # 3
-    st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state[:10])       # 4 (First 10 documents just for testing
-    st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) # 5
+    if "vector" not in st.session_state:
+        # Embeddings will be used elsewhere so good to use sessions
+        st.session_state.embeddings = OpenAIEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("./FOLDER_NAME")                                                # 1
+        st.session_state.docs = st.session_state.loader.load()                                                         # 2
+        # Need to also save the resulting chunks to session
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)            # 3
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state[:20])       # 4 (First 10 documents just for testing
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) # 5
 
-prmpt1 = st.text_input("Enter your Question (About the Documents)")
+
+
+prompt1 = st.text_input("Enter your Question (About the Documents)")
+
 if st.button("Embed Documents"):        # Will take documents from our folder, convert chunks, convert and store vectors, etc
-    #vector_embedding()
+    vector_embedding()
     st.write("Vector Store DB is Ready")
+
+# "read" vectors and perform q&a
+import time
+if prompt1:
+    start = time.process_time()
+    # take vectors and create document chain (context)
+    document_chain = create_stuff_documents_chain(llm,prompt)
